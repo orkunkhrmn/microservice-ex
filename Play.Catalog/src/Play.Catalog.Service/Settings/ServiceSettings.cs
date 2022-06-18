742d34e2a8332db96964116391a7b343d2f1b72f
orkunkhrmn <orkunkahraman@gmail.com>
2022-06-18 20:47:06 +0300

    Shared items replaced with new package as common

--- Play.Catalog/src/Play.Catalog.Service/Settings/ServiceSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace Play.Catalog.Service.Settings
-{
-    public class ServiceSettings
-    {
-        // TODO: init olduktan sonra değer değişmesin diye set yerine init yazdık
-        public string ServiceName { get; init; }
-    }
-}
