879b29f55204f2c12ce9462a9819a6e5371ba8e0
orkunkhrmn <orkunkahraman@gmail.com>
2022-06-19 14:32:11 +0300

    rabbitmq settings for masstransit configured

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Contracts;
 using Play.Catalog.Service.Dtos;
 using Play.Catalog.Service.Entities;
 using Play.Common;
@@ -14,35 +16,19 @@ namespace Play.Catalog.Service.Controllers
     public class ItemsController : ControllerBase
     {
         private readonly IRepository<Item> itemRepository;
-        private static int requestCounter = 0;
+        private readonly IPublishEndpoint publishEndpoint;
 
-        public ItemsController(IRepository<Item> itemRepository)
+        public ItemsController(IRepository<Item> itemRepository, IPublishEndpoint publishEndpoint)
         {
             this.itemRepository = itemRepository;
+            this.publishEndpoint = publishEndpoint;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ItemDto>>> GetAsync()
         {
-            requestCounter++;
-            Console.WriteLine($"Request {requestCounter}: Starting...");
-
-            if (requestCounter <= 2)
-            {
-                Console.WriteLine($"Request {requestCounter}: Delaying...");
-                await Task.Delay(TimeSpan.FromSeconds(10));
-            }
-
-            if (requestCounter <= 4)
-            {
-                Console.WriteLine($"Request {requestCounter}: 500(Internal Serval Error)...");
-                return StatusCode(500);
-            }
-
             var items = (await itemRepository.GetAllAsync())
                             .Select(item => item.AsDto());
-
-            Console.WriteLine($"Request {requestCounter}: 200(OK)...");
 
             return Ok(items);
         }
@@ -74,6 +60,8 @@ namespace Play.Catalog.Service.Controllers
 
             await itemRepository.CreateAsync(item);
 
+            await publishEndpoint.Publish(new CatalogItemCreated(item.Id, item.Name, item.Description));
+
             // TODO: Url Döner "location property" sadece yukarıya istek atmaz ve eklenen item ı döner
             return CreatedAtAction(nameof(GetByIdAsync), new { id = item.Id }, item);
         }
@@ -97,6 +85,8 @@ namespace Play.Catalog.Service.Controllers
 
             await itemRepository.UpdateAsync(existingItem);
 
+            await publishEndpoint.Publish(new CatalogItemUpdated(existingItem.Id, existingItem.Name, existingItem.Description));
+
             return NoContent();
         }
 
@@ -112,6 +102,8 @@ namespace Play.Catalog.Service.Controllers
             }
 
             await itemRepository.RemoveAsync(item.Id);
+
+            await publishEndpoint.Publish(new CatalogItemDeleted(id));
 
             return NoContent();
         }
